26ca0654c45ab5d5ad1c4594ef44895c20655eb6
Tirkish Baymyradov <garakchy@yahoo.com>
2024-12-23 20:10:25 +0300

    devam ediyoz

--- python_data/ders05/grafik2.py
+++ python_data/ders05/grafik2.py
@@ -2,3 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# takımların kazandıkları madalya türü ve sayısını bulalım
+# Example data for veri
+veri = pd.DataFrame({
+	"takim": ["Team A", "Team B", "Team C", "Team D"],
+	"madalya_Gold": [1, 2, 3, 4],
+	"madalya_Silver": [2, 1, 4, 3],
+	"madalya_Bronze": [3, 4, 1, 2]
+})
+
+mydata = veri[["takim", "madalya_Gold", "madalya_Silver", "madalya_Bronze"]].groupby(["takim"]).sum().sort_values(by="madalya_Gold", ascending=False)
+              
+print(mydata.head(10))

86ffcbf172c8d72594d50d125fafdb6012ed18e4
Tirkish Baymyradov <garakchy@yahoo.com>
2024-12-30 19:39:59 +0300

    zb uje

--- python_data/ders06/dosya2.py
+++ python_data/ders06/dosya2.py
@@ -1,5 +1,7 @@
+# mouse kontrol olayları ile şekil çizme ve mesaj yazma
 import turtle
 from random import randint as renk
+from datetime import datetime 
 
 turtle.screensize(800, 600)
 
@@ -28,24 +30,29 @@ def ucgen():
     turtle.end_fill()
     turtle.penup()
 
-def yazi():
+def yazi(bilgi, x, y):
+    turtle.color("#000")
     turtle.pendown()
+    turtle.goto(x, y)
     yaziFontu = ("Arial", 20, "bold")
     turtle.write(bilgi, font=yaziFontu)
     turtle.penup() 
 
-def mesaj():
+def mesaj(x, y):
     bilgi = turtle.textinput("Mesaj", "Bir mesaj yazın")
     print(bilgi)
     if bilgi == "quit" : quit()
-    else: yazi(bilgi)
+    else: yazi(bilgi, x, y)
 
 def uygulama():
     # turtle.onscreenclick(fun, btn) : mouse tıklannmasıyla x, y koordinatları alınır
     # btn, üç özelliği var (1, 2, 3) : 1 = sol, 2 = orta, 3 = sağ
-    turtle.onscreenclick(ucgen, btn=1)
+    turtle.onscreenclick(ucgen, btn=1) # sol tıklama
+    turtle.onscreenclick(kareciz, btn=3)   # sağ tıklama
+    turtle.onscreenclick(mesaj, btn=2)  # orta tıklama
 
-    turtle.listen()
+    turtle.listen() # klavye girişlerini dinle
 
+uygulama()
 kareciz()
 turtle.mainloop() # ekranı açık tut

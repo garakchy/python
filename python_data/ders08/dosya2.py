2ed7f690e84c26d0339b9a8b628daa700c12bd3d
Tirkish Baymyradov <garakchy@yahoo.com>
2025-01-06 18:50:12 +0300

    mesaj2

--- python_data/ders08/dosya2.py
+++ python_data/ders08/dosya2.py
@@ -1,5 +1,5 @@
 # dışarıdan girilen değere göre resim dosyasını boyutlandırma
-
+import numpy as np
 import cv2
 genislik = int(input("Genişlik değeri giriniz: "))
 yukseklik = int(input("Yükseklik değeri giriniz: "))
@@ -11,8 +11,9 @@ resim1 = cv2.imread("./resim1.jpg")
 # görserli okuma işleminde blue, green, red değerleri okunur
 print(resim1.shape) # y, x, boyut(ndim)
 gri_resim = cv2.cvtColor(resim1, cv2.COLOR_BGR2GRAY)
+boyutlandir = cv2.resize(gri_resim, (genislik, yukseklik))
 
-cv2.imshow("Gri Resim", gri_resim)
+cv2.imshow("Gri Resim", boyutlandir(gri_resim, x, y))
 
 if cv2.waitKey(0) & 0xFF == ord("q"):
     cv2.destroyAllWindows()

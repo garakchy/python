347f654ccee245b2eb0de56230c43c57264a0a32
Tirkish Baymyradov <garakchy@yahoo.com>
2025-02-03 18:59:57 +0300

    dosyaya bitiş komutlar eklendi

--- python_data/ders11/dosya1.py
+++ python_data/ders11/dosya1.py
@@ -14,3 +14,15 @@ uygulama.fill((255, 255, 255)) # Fill the window with white color
 zeminresmi = pygame.image.load("./image.jpg") # Load the background image
 
 kontrol = True
+while kontrol:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            kontrol = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                kontrol = False
+    uygulama.blit(zeminresmi, (0, 0)) # Draw the background image
+    pygame.display.update() # Update the window
+
+pygame.quit() # Quit pygame
+system("clear") # Clear the terminal

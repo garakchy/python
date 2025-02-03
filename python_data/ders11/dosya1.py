4c19578c1fb896074c243b5717c5f4d9fbcf33f7
Tirkish Baymyradov <garakchy@yahoo.com>
2025-02-03 18:46:44 +0300

    güncel dosya eklendi

--- python_data/ders11/dosya1.py
+++ python_data/ders11/dosya1.py
@@ -3,10 +3,14 @@ from os import system
 
 pygame.init() # Initialize pygame
 
-
-
-
-
 icon = pygame.image.load("./file.png") # Load the icon image
-pygame.display.set_icon
+pygame.display.set_icon(icon)
+
 pygame.display.set_caption("My Game") # Set the title of the window
+uygulama = pygame.display.set_mode((800, 600)) # Create the window
+
+uygulama.fill((255, 255, 255)) # Fill the window with white color
+
+zeminresmi = pygame.image.load("./image.jpg") # Load the background image
+
+kontrol = True

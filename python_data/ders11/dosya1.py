d3f0f767f949f6d33a9fbc20fa58e889ea47de17
Tirkish Baymyradov <garakchy@yahoo.com>
2025-02-03 18:25:18 +0300

    güncel dosya pushlandı

--- python_data/ders11/dosya1.py
+++ python_data/ders11/dosya1.py
@@ -2,3 +2,11 @@ import pygame
 from os import system
 
 pygame.init() # Initialize pygame
+
+
+
+
+
+icon = pygame.image.load("./file.png") # Load the icon image
+pygame.display.set_icon
+pygame.display.set_caption("My Game") # Set the title of the window
